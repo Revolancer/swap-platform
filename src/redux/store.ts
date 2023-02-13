e600e0967040f476dd5dbcae459619fcdd38348b
Skye Brady <skye@revolancer.com>
2023-02-13 10:35:52 +0000

    don't log

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,4 +9,3 @@ export default store;
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
-console.log(store.getState());
