348c1b03f1b5dc87ed13397703dd330409b87d11
Skye Brady <skye@revolancer.com>
2023-03-02 16:50:50 +0000

    set up chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import store from "@/redux/store";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import { darkTheme } from "stitches.config";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <div id="mainctx" className={"" /*darkTheme*/}>
-        <Component {...pageProps} />
-      </div>
+      <ChakraProvider>
+        <div id="mainctx" className={"" /*darkTheme*/}>
+          <Component {...pageProps} />
+        </div>
+      </ChakraProvider>
     </Provider>
   );
 }
