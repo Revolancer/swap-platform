0d4d66fc9948eb4171f8a01674839d101fdce738
Skye Brady <skye@revolancer.com>
2023-04-04 14:59:29 +0000

    custom tag input styles

--- src/components/forms/taginput.tsx
+++ src/components/forms/taginput.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Feedback } from "./feedback";
 import { InputOuter } from "./input";
 import { type Tag, WithContext as ReactTags } from "react-tag-input";
+import { styled } from "stitches.config";
 
 const KeyCodes = {
   comma: 188,
@@ -55,13 +56,16 @@ export const TagField = ({
         return (
           <>
             <InputOuter error={touched[name] && !!errors[name]}>
-              <ReactTags
-                handleDelete={handleDelete}
-                handleAddition={handleAddition}
-                handleDrag={handleDrag}
-                tags={tags}
-                suggestions={allTags}
-              />
+              <TagsContainer>
+                <ReactTags
+                  handleDelete={handleDelete}
+                  handleAddition={handleAddition}
+                  handleDrag={handleDrag}
+                  tags={tags}
+                  suggestions={allTags}
+                  autocomplete={true}
+                />
+              </TagsContainer>
             </InputOuter>
             {touched[name] && errors[name] && (
               <Feedback state="error">{errors[name]}</Feedback>
@@ -72,3 +76,38 @@ export const TagField = ({
     </Field>
   );
 };
+
+const TagsContainer = styled("div", {
+  "& .ReactTags__selected": {
+    display: "flex",
+    gap: "$3",
+    flexWrap: "wrap",
+    alignItems: "center",
+
+    "& input": {
+      border: "none",
+
+      "&:focus, &:focus-visible": {
+        outline: "none",
+      },
+    },
+
+    "& .ReactTags__tag": {
+      display: "flex",
+      gap: "$2",
+      border: "1px solid $pink500",
+      color: "$pink500",
+      padding: "$1 $2 $1 $4",
+      borderRadius: "$3",
+      background: "$background",
+
+      "& .ReactTags__remove": {
+        backgroundImage: "none",
+        background: "none",
+        border: "none",
+        cursor: "pointer",
+        color: "$pink500",
+      },
+    },
+  },
+});
