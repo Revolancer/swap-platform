7e31036f1a8686c8a79ae2090842e429e47dd9d5
Skye Brady <skye@revolancer.com>
2023-04-04 10:49:07 +0000

    tag input package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: [
+    "react-tag-input",
+    "react-dnd",
+    "dnd-core",
+    "@react-dnd/invariant",
+    "@react-dnd/asap",
+    "@react-dnd/shallowequal",
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
