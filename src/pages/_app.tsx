9b4c823054ce70e376160eb4d653dbf82b2b1d5c
Skye Brady <skye@revolancer.com>
2023-05-01 09:56:57 +0000

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import store from "@/redux/store";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { darkTheme } from "stitches.config";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <div id="mainctx" className={"" /*darkTheme*/}>
-        <Component {...pageProps} />
-      </div>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <div id="mainctx" className={"" /*darkTheme*/}>
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+      <Analytics />
+    </>
   );
 }
