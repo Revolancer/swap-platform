a64658874240996023ded0a7d04bed9e1abaf5b3
Skye Brady <skye@revolancer.com>
2023-05-18 08:36:31 +0000

    counts for new posts

--- src/pages/admin/stats.tsx
+++ src/pages/admin/stats.tsx
@@ -16,6 +16,15 @@ export default function Stats() {
   const [dauOverMau, setDauOverMau] = useState(0);
   const [dauOverWau, setDauOverWau] = useState(0);
   const [wauOverMau, setWauOverMau] = useState(0);
+  const [dailyPortfolios, setDailyPortfolios] = useState(0);
+  const [weeklyPortfolios, setWeeklyPortfolios] = useState(0);
+  const [monthlyPortfolios, setMonthlyPortfolios] = useState(0);
+  const [dailyNeeds, setDailyNeeds] = useState(0);
+  const [weeklyNeeds, setWeeklyNeeds] = useState(0);
+  const [monthlyNeeds, setMonthlyNeeds] = useState(0);
+  const [dailyProposals, setDailyProposals] = useState(0);
+  const [weeklyProposals, setWeeklyProposals] = useState(0);
+  const [monthlyProposals, setMonthlyProposals] = useState(0);
 
   const load = async () => {
     await axiosPrivate
@@ -33,6 +42,31 @@ export default function Stats() {
         setDauOverMau(dauOverMau);
         setDauOverWau(dauOverWau);
         setWauOverMau(wauOverMau);
+      })
+      .catch(() => {});
+    await axiosPrivate
+      .get("admin/stats/count_new_posts")
+      .then((response) => {
+        const {
+          dailyPortfolios,
+          dailyNeeds,
+          dailyProposals,
+          weeklyPortfolios,
+          weeklyNeeds,
+          weeklyProposals,
+          monthlyPortfolios,
+          monthlyNeeds,
+          monthlyProposals,
+        } = response.data;
+        setDailyPortfolios(dailyPortfolios);
+        setWeeklyPortfolios(weeklyPortfolios);
+        setMonthlyPortfolios(monthlyPortfolios);
+        setDailyNeeds(dailyNeeds);
+        setWeeklyNeeds(weeklyNeeds);
+        setMonthlyNeeds(monthlyNeeds);
+        setDailyProposals(dailyProposals);
+        setWeeklyProposals(weeklyProposals);
+        setMonthlyProposals(monthlyProposals);
       })
       .catch(() => {});
   };
@@ -55,6 +89,46 @@ export default function Stats() {
           <P>DAU/WAU: {dauOverWau}</P>
           <P>WAU/MAU: {wauOverMau}</P>
           <P>DAU/MAU: {dauOverMau}</P>
+          <H5>User Content</H5>
+          <P css={{ fontWeight: "$bold" }}>Daily</P>
+          <P>
+            Portfolios: {dailyPortfolios} (
+            {(dailyPortfolios / Math.max(1, dau)).toFixed(2)} of DAU)
+          </P>
+          <P>
+            Needs: {dailyNeeds} ({(dailyNeeds / Math.max(1, dau)).toFixed(2)} of
+            DAU)
+          </P>
+          <P>
+            Proposals: {dailyProposals} (
+            {(dailyProposals / Math.max(1, dau)).toFixed(2)} of DAU)
+          </P>
+          <P css={{ fontWeight: "$bold" }}>Weekly</P>
+          <P>
+            Portfolios: {weeklyPortfolios} (
+            {(weeklyPortfolios / Math.max(1, wau)).toFixed(2)} of @AU)
+          </P>
+          <P>
+            Needs: {weeklyNeeds} ({(weeklyNeeds / Math.max(1, wau)).toFixed(2)}{" "}
+            of @AU)
+          </P>
+          <P>
+            Proposals: {weeklyProposals} (
+            {(weeklyProposals / Math.max(1, wau)).toFixed(2)} of @AU)
+          </P>
+          <P css={{ fontWeight: "$bold" }}>Monthly</P>
+          <P>
+            Portfolios: {monthlyPortfolios} (
+            {(monthlyPortfolios / Math.max(1, mau)).toFixed(2)} of MAU)
+          </P>
+          <P>
+            Needs: {monthlyNeeds} (
+            {(monthlyNeeds / Math.max(1, mau)).toFixed(2)} of MAU)
+          </P>
+          <P>
+            Proposals: {monthlyProposals} (
+            {(monthlyProposals / Math.max(1, mau)).toFixed(2)} of MAU)
+          </P>
         </FullWidth>
       </AdminLayout>
     </>
