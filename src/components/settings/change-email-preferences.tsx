a4af279edba2fff2e6302a90785aaf14671ff5ab
Skye Brady <skye@revolancer.com>
2023-06-05 15:01:04 +0000

    Allow changing email preferences

---
+++ src/components/settings/change-email-preferences.tsx
@@ -0,0 +1,126 @@
+import { Flex } from "../layout/flex";
+import { axiosPrivate } from "@/lib/axios";
+import { Form } from "../forms/form";
+import { Formik } from "formik";
+import { Yup } from "@/lib/yup";
+import { Button } from "../navigation/button";
+import { Checkbox, InputInner, InputOuter } from "../forms/input";
+import { Feedback } from "../forms/feedback";
+import { H5 } from "../text/headings";
+import { useEffect, useState } from "react";
+import { SuccessModal } from "../navigation/success-modal";
+import { P } from "../text/text";
+
+export const ChangeEmailPreferences = () => {
+  const [firstParty, setFirstParty] = useState(false);
+  const [thirdParty, setThirdParty] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    axiosPrivate
+      .get("user/email_prefs")
+      .then((res) => res.data)
+      .then((data) => {
+        setFirstParty(data.firstparty);
+        setThirdParty(data.thirdparty);
+        setLoaded(true);
+      })
+      .catch((e) => setLoaded(true));
+  }, []);
+
+  if (!loaded) return <></>;
+  return (
+    <Formik
+      initialValues={{
+        marketingfirstparty: firstParty,
+        marketingthirdparty: thirdParty,
+      }}
+      onSubmit={async (values, actions) => {
+        actions.setSubmitting(true);
+        await axiosPrivate
+          .post("user/email_prefs", values)
+          .then(() => {
+            setSuccess(true);
+          })
+          .catch((reason) => {
+            if (reason.code == "ERR_NETWORK") {
+              actions.setFieldError(
+                "marketingthirdparty",
+                "Oops, something went wrong"
+              );
+            } else {
+              const statuscode = Number(reason?.response?.status);
+              switch (statuscode) {
+                default:
+                  actions.setFieldError(
+                    "marketingthirdparty",
+                    "Something went wrong"
+                  );
+                  break;
+              }
+            }
+          });
+        actions.setSubmitting(false);
+      }}
+    >
+      {(props) => {
+        return (
+          <Form onSubmit={props.handleSubmit} css={{ gap: "$3" }}>
+            <Flex column>
+              <Checkbox
+                id="marketingfirstparty"
+                name="marketingfirstparty"
+                checked={props.values.marketingfirstparty}
+              >
+                I would like to receive updates about Revolancer
+              </Checkbox>
+              {props.touched.marketingfirstparty &&
+                props.errors.marketingfirstparty && (
+                  <Feedback state="error">
+                    {props.errors.marketingfirstparty}
+                  </Feedback>
+                )}
+              <Checkbox
+                id="marketingthirdparty"
+                name="marketingthirdparty"
+                checked={props.values.marketingthirdparty}
+              >
+                I would like to receive updates about Revolancer&rsquo;s
+                partners
+              </Checkbox>
+              {props.touched.marketingthirdparty &&
+                props.errors.marketingthirdparty && (
+                  <Feedback state="error">
+                    {props.errors.marketingthirdparty}
+                  </Feedback>
+                )}
+            </Flex>
+            <P css={{ color: "$neutral600", fontStyle: "italic" }}>
+              Please note, some emails are required for the functionality of the
+              site and cannot be disabled without closing your Revolancer
+              account.
+            </P>
+            <Button
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                props.submitForm();
+              }}
+            >
+              Save
+            </Button>
+            {success && (
+              <SuccessModal
+                successMessage="Your email preferences have been updated"
+                onClose={() => {
+                  setSuccess(false);
+                }}
+              />
+            )}
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
