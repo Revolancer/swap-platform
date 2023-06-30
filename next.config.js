14ddea5ec1d281858e931b5e96901ff6afd81669
Skye Brady <skye@revolancer.com>
2023-06-30 14:50:36 +0000

    Admin page to delete user

--- next.config.js
+++ next.config.js
@@ -16,6 +16,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "app.revolancer.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
