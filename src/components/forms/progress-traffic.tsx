ac042b56c3ff02136a61b706afd103c62cf035f9
Skye Brady <skye@revolancer.com>
2023-07-06 13:38:35 +0000

    trafficlight progress bar

---
+++ src/components/forms/progress-traffic.tsx
@@ -0,0 +1,71 @@
+import { useMemo } from "react";
+import { darkTheme, styled, config } from "stitches.config";
+
+const Outer = styled("div", {
+  position: "relative",
+  width: "100%",
+  height: "$5",
+  borderRadius: "$3",
+  overflow: "hidden",
+});
+
+const BG = styled("div", {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  backgroundColor: "$neutral300",
+
+  [`.${darkTheme} &`]: {
+    backgroundColor: "$neutral700",
+  },
+});
+
+const Inner = styled("div", {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  height: "100%",
+  borderRadius: "$3",
+});
+
+export const ProgressTraffic = ({ progress = 0 }: { progress?: number }) => {
+  progress = Math.min(progress, 100);
+  progress = Math.max(progress, 0);
+
+  const color = useMemo(() => {
+    if (progress >= 100) {
+      return config.theme.colors.green700;
+    }
+    if (progress >= 90) {
+      return config.theme.colors.green600;
+    }
+    if (progress >= 80) {
+      return config.theme.colors.green500;
+    }
+    if (progress >= 70) {
+      return config.theme.colors.green400;
+    }
+    if (progress >= 60) {
+      return config.theme.colors.yellow600;
+    }
+    if (progress >= 50) {
+      return config.theme.colors.orange300;
+    }
+    if (progress >= 40) {
+      return config.theme.colors.orange400;
+    }
+    if (progress >= 35) {
+      return config.theme.colors.orange600;
+    }
+    return config.theme.colors.red500;
+  }, [progress]);
+
+  return (
+    <Outer>
+      <BG />
+      <Inner css={{ width: `${progress}%`, backgroundColor: color }} />
+    </Outer>
+  );
+};
