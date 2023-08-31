d6e53ad24ce77fd472a2829464b87524fa1832fd
Skye Brady <skye@revolancer.com>
2023-08-31 14:21:51 +0000

    Allow user to cancel

--- src/components/project-hub/active/project-completion-toggle.tsx
+++ src/components/project-hub/active/project-completion-toggle.tsx
@@ -1,52 +1,28 @@
 import { Button } from '@revolancer/ui/buttons';
 import { axiosPrivate } from '@/lib/axios';
-import { Project, UserProfileData } from '@/lib/types';
+import { Project } from '@/lib/types';
 import store from '@/redux/store';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Flex } from '@revolancer/ui/layout';
-import { P } from '@revolancer/ui/text';
+import { Span } from '@revolancer/ui/text';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck,
+  faCircleXmark,
+  faCirclePause,
+} from '@fortawesome/free-regular-svg-icons';
 
-export const ProjectCompletionToggle = ({ project }: { project: Project }) => {
-  const [otherProfile, setOtherProfile] = useState<UserProfileData>();
-  const [hasApproved, setHasApproved] = useState(false);
-  const [isComplete, setIsComplete] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const [otherHasApproved, setOtherHasApproved] = useState(false);
-
+const ApprovalButton = ({
+  approved,
+  disabled,
+  project,
+}: {
+  approved: boolean;
+  disabled: boolean;
+  project: Project;
+}) => {
   const router = useRouter();
-
-  useEffect(() => {
-    const self = store?.getState()?.userData?.user?.id ?? '';
-    const isClient = project.client.id == self;
-    setHasApproved(
-      isClient ? project.client_approval : project.contractor_approval,
-    );
-    setOtherHasApproved(
-      !isClient ? project.client_approval : project.contractor_approval,
-    );
-    const otherUserID = isClient ? project.contractor.id : project.client.id;
-    const loadProfile = async (id: string) => {
-      if (id == '') return;
-      return await axiosPrivate
-        .get(`user/profile/by_id/${id}`)
-        .then((res) => res.data)
-        .then((data) => {
-          if ((data?.id ?? false) !== false) {
-            setOtherProfile(data);
-            setIsLoading(false);
-          }
-        })
-        .catch((err) => {
-          return;
-        });
-    };
-    loadProfile(otherUserID);
-    if (project.status == 'complete') {
-      setIsComplete(true);
-      setIsLoading(false);
-    }
-  }, [project]);
 
   const markComplete = () => {
     axiosPrivate
@@ -62,9 +38,7 @@ export const ProjectCompletionToggle = ({ project }: { project: Project }) => {
       .catch(() => {});
   };
 
-  if (isLoading) return <></>;
-  if (isComplete) return <></>;
-  if (!hasApproved)
+  if (approved) {
     return (
       <Button
         href="#"
@@ -75,21 +49,116 @@ export const ProjectCompletionToggle = ({ project }: { project: Project }) => {
           markComplete();
         }}
       >
+        <Span css={{ color: '$green500' }}>
+          <FontAwesomeIcon icon={faCircleCheck} />
+        </Span>{' '}
         Mark Complete
       </Button>
     );
+  } else {
+    return (
+      <Button
+        href="#"
+        role="secondary"
+        size={'small'}
+        onClick={(e) => {
+          e.preventDefault();
+          markIncomplete();
+        }}
+        disabled={disabled}
+      >
+        <Span css={{ color: '$orange500' }}>
+          <FontAwesomeIcon icon={faCirclePause} />
+        </Span>{' '}
+        Mark Incomplete
+      </Button>
+    );
+  }
+};
 
+const CancelButton = ({
+  pendingOwn,
+  project,
+}: {
+  pendingOwn: boolean;
+  project: Project;
+}) => {
+  const router = useRouter();
+
+  const requestCancellation = () => {
+    axiosPrivate
+      .put(`projects/cancellation/${project.id}`)
+      .then(() => router.reload())
+      .catch(() => {});
+  };
+
+  if (pendingOwn) {
+    return (
+      <Button href="#" role="secondary" size={'small'} disabled>
+        Cancellation Requested
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        href="#"
+        role="secondary"
+        size={'small'}
+        onClick={(e) => {
+          e.preventDefault();
+          requestCancellation();
+        }}
+      >
+        <Span css={{ color: '$red500' }}>
+          <FontAwesomeIcon icon={faCircleXmark} />
+        </Span>{' '}
+        Cancel
+      </Button>
+    );
+  }
+};
+
+export const ProjectCompletionToggle = ({ project }: { project: Project }) => {
+  const [hasApproved, setHasApproved] = useState(false);
+  const [hasCancelled, setHasCancelled] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [otherHasCancelled, setOtherHasCancelled] = useState(false);
+
+  useEffect(() => {
+    const self = store?.getState()?.userData?.user?.id ?? '';
+    const isClient = project.client.id == self;
+    setHasApproved(
+      isClient ? project.client_approval : project.contractor_approval,
+    );
+    setHasCancelled(
+      isClient ? project.client_cancellation : project.contractor_cancellation,
+    );
+    setOtherHasCancelled(
+      !isClient ? project.client_cancellation : project.contractor_cancellation,
+    );
+    if (project.status == 'complete') {
+      setIsComplete(true);
+    }
+    setIsLoading(false);
+  }, [project]);
+
+  if (isLoading)
+    return (
+      <Flex>
+        <Button href="#" loading />
+        <Button href="#" loading />
+      </Flex>
+    );
+  if (isComplete) return <></>;
   return (
-    <Button
-      href="#"
-      role="secondary"
-      size={'small'}
-      onClick={(e) => {
-        e.preventDefault();
-        markIncomplete();
-      }}
-    >
-      Mark Not Complete
-    </Button>
+    <Flex>
+      <ApprovalButton
+        project={project}
+        approved={hasApproved}
+        disabled={otherHasCancelled || hasCancelled}
+      />
+      <CancelButton project={project} pendingOwn={hasCancelled} />
+    </Flex>
   );
 };
