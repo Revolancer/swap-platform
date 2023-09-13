3d21a22d135e256541c86a97b58456e10f4d2f00
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-09-13 05:22:25 +0000

    add skeleton UI for profile pages

--- src/pages/u/[slug].tsx
+++ src/pages/u/[slug].tsx
@@ -25,7 +25,10 @@ import {
   MainContentWithSideBar,
   SideBar,
 } from '@revolancer/ui/layout';
-import { H1 } from '@revolancer/ui/text';
+import { H1, P } from '@revolancer/ui/text';
+import { SkeletonText } from '@revolancer/ui/skeleton';
+import { RoundedSquareImage } from '@revolancer/ui/user';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
 export default function UserProfile() {
   const router = useRouter();
@@ -33,6 +36,7 @@ export default function UserProfile() {
   const [userProfile, setUserProfile] = useState<UserProfileData>({});
   const [own, setOwn] = useState(false);
   const [isNotFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUserProfileData = async () => {
@@ -69,12 +73,187 @@ export default function UserProfile() {
             setNotFound(true);
           });
       }
+      setLoading(false);
     };
     getUserProfileData();
   }, [slug, userProfile?.user?.id]);
+
   if (isNotFound) {
     return <FourOhFour />;
   }
+
+  const NeedsSkeleton = () => (
+    <Card unpadded>
+      <SkeletonText />
+      <Flex column gap={4} css={{ padding: '$6' }}>
+        <SkeletonText
+          css={{
+            fontWeight: '$bold',
+            fontSize: '$body1',
+            lineHeight: '$body1',
+          }}
+          type="p"
+        />
+        <Flex css={{ alignItems: 'center' }}>
+          {/*<RoundedSquareImage loading size="small" />*/}
+          <SkeletonText
+            css={{
+              width: '$9',
+              height: '$9',
+              borderRadius: '$2',
+            }}
+          />
+          <SkeletonText type="p" />
+        </Flex>
+        <Flex>
+          {Array(3)
+            .fill(null)
+            .map((item, idx) => (
+              <SkeletonText type="tag" key={`tag-${idx}`} />
+            ))}
+        </Flex>
+        {Array(3)
+          .fill(null)
+          .map((item, idx) => (
+            <SkeletonText type="p" key={`p-${idx}`} />
+          ))}
+      </Flex>
+    </Card>
+  );
+
+  const skeletonArray = Array(15).fill(<NeedsSkeleton />);
+
+  const Skeleton = () => (
+    <>
+      <SideBar>
+        <Card css={{ overflow: 'unset' }}>
+          <Flex
+            column
+            gap={3}
+            css={{
+              alignItems: 'center',
+            }}
+          >
+            <RoundedSquareImage loading size="xl" />
+            <SkeletonText type="h1" />
+          </Flex>
+          <Flex
+            style={{
+              justifyContent: 'flex-start',
+              width: '100%',
+            }}
+          >
+            <P css={{ color: '$neutral600' }}>About</P>
+          </Flex>
+          {Array(3)
+            .fill(null)
+            .map((item, idx) => (
+              <SkeletonText
+                type="p"
+                key={`p-${idx}`}
+                css={{ marginTop: '$2' }}
+              />
+            ))}
+          <SkeletonText type="p" css={{ width: '25%', marginTop: '$2' }} />
+          <Flex
+            style={{
+              justifyContent: 'flex-start',
+              width: '100%',
+            }}
+          >
+            <P css={{ color: '$neutral600' }}>Socials</P>
+          </Flex>
+          <Flex gap={4}>
+            {Array(4)
+              .fill(null)
+              .map((item, idx) => (
+                <RoundedSquareImage loading size="small" key={idx.toString()} />
+              ))}
+          </Flex>
+          <Flex
+            style={{
+              justifyContent: 'flex-start',
+              width: '100%',
+            }}
+          >
+            <P css={{ color: '$neutral600' }}>Skills</P>
+          </Flex>
+          <Flex>
+            {Array(4)
+              .fill(null)
+              .map((item, idx) => (
+                <SkeletonText type="tag" key={`tag-${idx}`} />
+              ))}
+          </Flex>
+        </Card>
+      </SideBar>
+      <MainContentWithSideBar>
+        <Flex column gap={8}>
+          <SkeletonText type="h2" css={{ height: '$17' }} />
+          <Flex column gap={4}>
+            <SkeletonText type="h5" />
+            <ResponsiveMasonry columnsCountBreakPoints={{ 0: 1, 1200: 2 }}>
+              <Masonry gutter="0.8rem">{skeletonArray}</Masonry>
+            </ResponsiveMasonry>
+          </Flex>
+        </Flex>
+      </MainContentWithSideBar>
+    </>
+  );
+
+  const MainContent = () => (
+    <>
+      <SideBar>
+        {own && <ProfileProgress position="mobile" />}
+        <Card css={{ overflow: 'unset' }}>
+          <Flex
+            column
+            gap={3}
+            css={{
+              alignItems: 'center',
+            }}
+          >
+            <ProfileImage uid={userProfile?.user?.id ?? ''} own={own} />
+            <H1 css={{ fontSize: '$h4', lineHeight: '$h4' }}>
+              {userProfile?.first_name
+                ? `${userProfile?.first_name} ${userProfile?.last_name}`
+                : 'User Profile'}
+            </H1>
+            {!own && userProfile?.user?.id && (
+              <Button role="secondary" href={`/message/${userProfile.user.id}`}>
+                <FontAwesomeIcon icon={faMessage} /> Message
+              </Button>
+            )}
+          </Flex>
+          <AboutSegment
+            uid={userProfile?.user?.id ?? ''}
+            own={own}
+            loading={loading}
+          />
+          <Timezone uid={userProfile?.user?.id ?? ''} own={own} />
+          <SocialSegment uid={userProfile?.user?.id ?? ''} own={own} />
+          <SkillSegment uid={userProfile?.user?.id ?? ''} own={own} />
+        </Card>
+      </SideBar>
+      <MainContentWithSideBar>
+        <Flex column gap={8}>
+          <Tagline uid={userProfile?.user?.id ?? ''} own={own} />
+          {own && <ProfileProgress />}
+          <NeedsSegment
+            name={userProfile?.first_name ?? ''}
+            uid={userProfile?.user?.id ?? ''}
+            own={own}
+          />
+          <PortfolioSegment
+            name={userProfile?.first_name ?? ''}
+            uid={userProfile?.user?.id ?? ''}
+            own={own}
+          />
+        </Flex>
+      </MainContentWithSideBar>
+    </>
+  );
+
   return (
     <>
       <Title>
@@ -91,53 +270,7 @@ export default function UserProfile() {
               : 'User Profile'}
           </Crumb>
         </CrumbBar>
-        <SideBar>
-          {own && <ProfileProgress position="mobile" />}
-          <Card css={{ overflow: 'unset' }}>
-            <Flex
-              column
-              gap={3}
-              css={{
-                alignItems: 'center',
-              }}
-            >
-              <ProfileImage uid={userProfile?.user?.id ?? ''} own={own} />
-              <H1 css={{ fontSize: '$h4', lineHeight: '$h4' }}>
-                {userProfile?.first_name
-                  ? `${userProfile?.first_name} ${userProfile?.last_name}`
-                  : 'User Profile'}
-              </H1>
-              {!own && userProfile?.user?.id && (
-                <Button
-                  role="secondary"
-                  href={`/message/${userProfile.user.id}`}
-                >
-                  <FontAwesomeIcon icon={faMessage} /> Message
-                </Button>
-              )}
-            </Flex>
-            <AboutSegment uid={userProfile?.user?.id ?? ''} own={own} />
-            <Timezone uid={userProfile?.user?.id ?? ''} own={own} />
-            <SocialSegment uid={userProfile?.user?.id ?? ''} own={own} />
-            <SkillSegment uid={userProfile?.user?.id ?? ''} own={own} />
-          </Card>
-        </SideBar>
-        <MainContentWithSideBar>
-          <Flex column gap={8}>
-            <Tagline uid={userProfile?.user?.id ?? ''} own={own} />
-            {own && <ProfileProgress />}
-            <NeedsSegment
-              name={userProfile?.first_name ?? ''}
-              uid={userProfile?.user?.id ?? ''}
-              own={own}
-            />
-            <PortfolioSegment
-              name={userProfile?.first_name ?? ''}
-              uid={userProfile?.user?.id ?? ''}
-              own={own}
-            />
-          </Flex>
-        </MainContentWithSideBar>
+        {loading ? <Skeleton /> : <MainContent />}
       </PrimaryLayout>
     </>
   );
