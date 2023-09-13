07b3fc8076d033d8fa18262bb8ad28eef67d818f
Skye Brady <skye@revolancer.com>
2023-09-13 14:16:28 +0000

    Create user management pages & layout

---
+++ src/components/admin/user/tabs.tsx
@@ -0,0 +1,32 @@
+import { Tabs } from '@revolancer/ui/navigation';
+import { useRouter } from 'next/router';
+
+export const ManageUserTabs = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const routes = {
+    root: {
+      [`/admin/users/${id}`]: 'User Profile',
+    },
+    paths: {
+      settings: 'Account Settings',
+      wallet: 'Wallet',
+      posts: 'User Posts',
+      email_prefs: 'Preferences',
+      messages: 'Messages',
+      projects: 'Active Projects',
+      projects_completed: 'Completed Projects',
+    },
+  };
+
+  return (
+    <>
+      <Tabs
+        root={Object.keys(routes.root)[0]}
+        rootName={Object.values(routes.root)[0]}
+        routes={Object.keys(routes.paths)}
+        routeNames={Object.values(routes.paths)}
+      />
+    </>
+  );
+};
