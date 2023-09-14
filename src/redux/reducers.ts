5a7567599a03cd051526860baad556a9e5edf554
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-09-14 03:53:17 +0000

    remove loading reducer

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,16 +1,13 @@
 import navToggle from '@/components/navigation/main/nav-toggle';
 import adminToggle from '@/components/navigation/main/admin-toggle';
-import loadingToggle from '@/lib/loading';
 import { userSlice } from '@/lib/user/auth';
 import { combineReducers } from '@reduxjs/toolkit';
 
 const navigation = combineReducers({ toggle: navToggle });
 const admin = combineReducers({ toggle: adminToggle });
-const loading = combineReducers({ toggle: loadingToggle });
 
 export const rootReducer = combineReducers({
   navigation: navigation,
   admin: admin,
-  loading: loading,
   userData: userSlice.reducer,
 });
