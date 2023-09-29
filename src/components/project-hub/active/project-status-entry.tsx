02aedb91c44c68c60cf684cba5f3f05d683c0de2
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-09-29 06:52:23 +0000

    add new project table components

---
+++ src/components/project-hub/active/project-status-entry.tsx
@@ -0,0 +1,61 @@
+import type { Project } from '@/lib/types';
+import { styled } from '@revolancer/ui';
+import { Flex } from '@revolancer/ui/layout';
+
+export const ProjectStatusEntry = ({ project }: { project: Project }) => {
+  const Indicator = styled('div', {
+    width: '$4',
+    height: '$4',
+    borderRadius: '100%',
+    variants: {
+      status: {
+        cancelled: {
+          backgroundColor: '$red500',
+        },
+        active: {
+          backgroundColor: '$orange500',
+        },
+        complete: {
+          backgroundColor: '$green500',
+        },
+      },
+    },
+  });
+
+  const Text = styled('p', {
+    variants: {
+      status: {
+        cancelled: {
+          backgroundColor: '$red500',
+        },
+        active: {
+          color: '$orange500',
+        },
+        complete: {
+          color: '$green500',
+        },
+      },
+    },
+  });
+
+  const status = () => {
+    if (project.outcome) {
+      return project.outcome === 'cancelled' ? 'cancelled' : 'complete';
+    }
+    return project.status;
+  };
+
+  const statusText = () => {
+    if (project.outcome) {
+      return project.outcome === 'cancelled' ? 'Cancelled' : 'Completed';
+    }
+    return project.status === 'active' ? 'In Progress' : 'Completed';
+  };
+
+  return (
+    <Flex css={{ alignItems: 'center' }}>
+      <Indicator status={status()} />
+      <Text status={status()}>{statusText()}</Text>
+    </Flex>
+  );
+};
