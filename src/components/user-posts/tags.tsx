bb78fec04d9b88b819706490239e6e531f534646
Alfonso Vittorio Salvador <86940949+ponkeeto@users.noreply.github.com>
2023-10-10 05:19:40 +0000

    edit tags component to have tags expander

--- src/components/user-posts/tags.tsx
+++ src/components/user-posts/tags.tsx
@@ -1,11 +1,49 @@
 import { Tag } from '@/lib/types';
 import { TagElement } from '../tags';
 import { Flex } from '@revolancer/ui/layout';
+import { useState } from 'react';
+import { styled } from '@revolancer/ui';
+import { TertiaryFormButton } from '@revolancer/ui/buttons';
 
-export const Tags = ({ tags = [] }: { tags: Tag[] }) => {
-  const staticTags = [];
-  for (const tag of tags) {
-    staticTags.push(<TagElement tag={tag} key={tag.id} />);
-  }
-  return <Flex wrap>{staticTags}</Flex>;
+export const Tags = ({
+  tags = [],
+  expander = true,
+}: {
+  tags: Tag[];
+  expander?: boolean;
+}) => {
+  const [expand, setExpand] = useState(false);
+
+  const TagContainer = styled('div', {
+    color: '$pink500',
+    padding: '$1 $4',
+    borderRadius: '100px',
+    background: '$pink100',
+  });
+
+  const renderTags = tags.map((tag, idx) => {
+    if (idx < 4) {
+      return <TagElement tag={tag} key={tag.id} />;
+    } else {
+      return expand && <TagElement tag={tag} key={tag.id} />;
+    }
+  });
+  const showExpander = expander && !expand && tags.length > 5;
+  return (
+    <>
+      <Flex wrap>
+        {renderTags}
+        {showExpander && (
+          <TertiaryFormButton onClick={() => setExpand(true)}>
+            <TagContainer>+{tags.length - 4}</TagContainer>
+          </TertiaryFormButton>
+        )}
+      </Flex>
+      {expand && (
+        <TertiaryFormButton onClick={() => setExpand(false)}>
+          Show Less
+        </TertiaryFormButton>
+      )}
+    </>
+  );
 };
